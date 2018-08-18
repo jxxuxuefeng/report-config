943206741d1b4fab6102c437603e5c3141775e64
jxxuxuefeng <jxxuxuefeng@qq.com>
2018-08-18 12:08:08 +0800

    初始化项目

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        初始化项目
+      </div>
+    );
+  }
+}
+
+export default App;
